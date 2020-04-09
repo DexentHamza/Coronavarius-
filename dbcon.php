49809133dd5bcafb54e291b6df83216eb9c990fe
Dexent Hamza <Hamza.hamdan555@gmail.com>
2020-04-09 14:52:32 +0500

    Complete Project Corona

---
+++ dbcon.php
@@ -0,0 +1,30 @@
+<?php
+
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'corona';
+
+$con = mysqli_connect($server, $username, $password, $db);
+
+// if($con){
+//     ?>
+    <script>
+//         alert('connection successfull');
+//     </script>
+    <?php
+// }
+
+// else{
+//     ?>
+    <script>
+//         alert('not connection successfull');
+//     </script>
+    <?php
+// }
+
+
+
+
+
+?>
---
+++ link/link.php
@@ -0,0 +1,26 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js" integrity="sha256-w49DIDk8itgSjjuoof5lTxYKwcjWJ7Z2sRW3nU1StNA=" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js" integrity="sha256-JtQPj/3xub8oapVMaIijPNoM0DHoAtgh/gwFYuN5rik=" crossorigin="anonymous"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- <link href="/bootstrap-4.0.0/css/bootstrap.min.css"> -->
+
+<!-- <link href="/bootstrap-4.0.0/js/bootstrap.min.js"> -->
+
+<!-- Sweet Alert -->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
